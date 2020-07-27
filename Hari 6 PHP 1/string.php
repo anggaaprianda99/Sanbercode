6c61ff78b3758a1f41679ba36bc50dd7dcecd147
anggaaprianda99 <54490918+anggaaprianda99@users.noreply.github.com>
2020-07-28 00:18:13 +0700

    perbaiki baris baru

--- Hari 6 PHP 1/string.php
+++ Hari 6 PHP 1/string.php
@@ -29,10 +29,11 @@
         echo("<br>");
         echo("Panjang Kata adalah ");
         echo str_word_count($first_sentence);
+        echo("<br><br>");
 
 
         $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
-         echo($second_sentence);
+        echo($second_sentence);
         echo("<br>");
         echo("Panjang String adalah ");
         echo strlen($second_sentence);
